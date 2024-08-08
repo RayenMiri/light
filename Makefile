11b27cfc1969d39199ae8d5cd866c153f8f64991
RayenMiri <rayenmiri000@gmail.com>
2024-08-08 07:07:07 +0100

    make file changes to support only .light files

--- Makefile
+++ Makefile
@@ -4,6 +4,18 @@ OBJECTS := $(SOURCES:.c=.o)
 FLAGS := -g
 EXEC := light.exe
 
+# Default target to build and run the executable with a specified file
+run: $(EXEC)
+	@if not exist "$(file)" ( \
+		echo "Error: The file '$(file)' does not exist"; \
+		exit 1; \
+	)
+	@powershell -Command "if ('$(file)'.Substring('$(file)'.Length - 6) -ne '.light') { exit 1 }" || ( \
+		echo "Error: Only .light files are supported for execution"; \
+		exit 1; \
+	)
+	.\$(EXEC) $(file)
+
 # Target to build the executable
 $(EXEC): $(OBJECTS)
 	gcc $(OBJECTS) $(FLAGS) -o $(EXEC)
--- src/main.c
+++ src/main.c
@@ -1,26 +1,41 @@
+#include <stdio.h>
+#include <string.h>
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
 #include "include/io.h"
-#include <string.h>
-#include <stdio.h>
+
+#define MAX_LIMIt 20
 
 
-int main(int argc , char* argv[]){
-
-    char* file_content = get_file_content(argv[1]);
-   
-    lexer_t* lexer = init_lexer(
-      file_content
-
-    );
-    parser_t* parser = init_parser(lexer);
-
-    ast_t* root = parser_parse(parser,parser->scope);
-  
-    visitor_t* visitor = init_visitor();
-    
-    visitor_visit(visitor,root);
-   
+int main(int argc, char* argv[])
+{
+    if (argc >= 2){
+        for(int i = 1; i < argc; i++){
+            int len = strlen(argv[i]);
+            char* last_four = &argv[i][len-6];
+            if(strcmp(last_four,".light") == 0){
+                                lexer_t* lexer = init_lexer(
+                    get_file_content(argv[i])
+                );
+                parser_t* parser = init_parser(lexer);
+                ast_t* root = parser_parse(parser, parser->scope);
+                visitor_t* visitor = init_visitor();
+                visitor_visit(visitor, root);
+            }
+        }
+    }
+    else {
+        char input[MAX_LIMIt];
+        while(1){
+            printf("Welcome to the hello language v. 0.0.1!\nCreated by sebbekarlsson\n>>> ");
+            fgets(input,MAX_LIMIt, stdin);
+            lexer_t* lexer = init_lexer(input);
+            parser_t* parser = init_parser(lexer);
+            ast_t* root = parser_parse(parser, parser->scope);
+            visitor_t* visitor = init_visitor();
+            visitor_visit(visitor, root);
+        }
+    }
     return 0;
 }
